20159341823b258418e68578be4074cd986d1b7d
Jesus Bueno <jbuenope@emeal.nttdata.com>
2026-02-23 12:57:08 +0100

    Se ha annadido sin tocar nada la APi de Portal

---
+++ microservicio-Portal/src/main/java/com/festibank/microservicio_Portal/MicroservicioPortalApplication.java
@@ -0,0 +1,13 @@
+package com.festibank.microservicio_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPortalApplication.class, args);
+	}
+
+}
---
+++ microservicio-Portal/src/test/java/com/festibank/microservicio_Portal/MicroservicioPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festibank.microservicio_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
